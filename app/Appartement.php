2131c5cf7377e0c837e68baa165dafc6f4fd4cdf
jtanau <jmtanau18@gmail.com>
2021-04-29 23:04:31 +0200

    test github

--- app/Appartement.php
+++ app/Appartement.php
@@ -6,6 +6,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Appartement extends Model{
+/*test essai github*/
 
     protected $fillable = [
         'id', 'libelle','superficie','reference','commentaire','statut','prix','reference_logement'
